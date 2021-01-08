5942029add9662af6e5933cd91666c7fc774ec8b
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-01-08 18:41:25 +0000

    Fetching data with useEffect and deleted local delete blog functionality

--- src/BlogList.js
+++ src/BlogList.js
@@ -1,11 +1,10 @@
-const BlogList = ({ blogs, title, handleDelete }) => {
+const BlogList = ({ blogs, title }) => {
   return (
     <div className='blog-list'>
       {blogs.map((blog) => (
         <div className='blog-preview' key={blog.id}>
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
-          <button onClick={() => handleDelete(blog.id)}>Delete Blog</button>
         </div>
       ))}
     </div>
--- src/Home.js
+++ src/Home.js
@@ -4,22 +4,19 @@ import BlogList from './BlogList';
 const Home = () => {
   const [blogs, setBlogs] = useState(null);
 
-  const handleDelete = (id) => {
-    const newBlogs = blogs.filter((blog) => blog.id !== id);
-    setBlogs(newBlogs);
-  };
-
   useEffect(() => {
     fetch('http://localhost:8000/blogs')
       .then((res) => {
         return res.json();
       })
-      .then((data) => {});
+      .then((data) => {
+        setBlogs(data);
+      });
   }, []);
 
   return (
     <div className='home'>
-      <BlogList blogs={blogs} title='All Blogs!' handleDelete={handleDelete} />
+      {blogs && <BlogList blogs={blogs} title='All Blogs!' />}
     </div>
   );
 };
