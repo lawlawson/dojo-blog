e451dfc3be4c46619e5a495a3fde807506461407
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-01-08 18:33:26 +0000

    Added data folder with db.json file

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,7 @@ import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-    { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-    {
-      title: 'Web dev top tips',
-      body: 'lorem ipsum...',
-      author: 'mario',
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlogs] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
@@ -19,8 +10,12 @@ const Home = () => {
   };
 
   useEffect(() => {
-    console.log('Use effect ran');
-  });
+    fetch('http://localhost:8000/blogs')
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {});
+  }, []);
 
   return (
     <div className='home'>
