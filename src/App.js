468391eadd7f77834bf7c38a338f6df594578507
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-01-15 22:48:12 +0000

    Fixed syntax error with import statement

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import Navbar from './Navbar';
 import Home from './Home';
-import { BrowseRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
