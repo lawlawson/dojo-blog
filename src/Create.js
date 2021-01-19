dbb8af3fd0b8cc8d99c4a7cb58258cd7cf13d804
Lawrence Narh-Lawson <lawrencen@hotmail.com>
2021-01-19 18:46:29 +0000

    Added ability to edit title and body fields

--- src/Create.js
+++ src/Create.js
@@ -1,18 +1,34 @@
+import { useState } from 'react';
+
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+
   return (
     <div className='create'>
       <h2>Add A New Blog</h2>
       <form>
         <label>Blog Title</label>
-        <input type='text' required />
+        <input
+          type='text'
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
         <label>Blog Body: </label>
-        <textarea required></textarea>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        ></textarea>
         <label>Blog Author:</label>
         <select>
           <option value='mario'>Mario</option>
           <option value='yoshi'>Yoshi</option>
         </select>
         <button>Add Blog</button>
+        <p>{title}</p>
+        <p>{body}</p>
       </form>
     </div>
   );
